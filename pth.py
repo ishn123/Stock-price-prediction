d4fb64eb4916c684c387b5c6c824058c26b314dc
Anant2410 <anantgarg2410@gmail.com>
2023-01-15 00:51:12 +0530

    first commit

---
+++ pth.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pandas_datareader as data
+import streamlit as st
+from keras.models import load_model
+import yfinance as yf
+
+start = '2012-01-01'
+end = '2022-10-08'
+
+st.title('Stock Prices Prediction')
+
+stock_symbol = st.text_input('Enter Stock Ticker' , 'AAPL')
+df = yf.download(tickers = stock_symbol, start = start, end=end )
+
+# describing data
+st.subheader('Data From 2012 - 2022')
+st.write(df.describe())
+
+#visualizaions
+st.subheader('closing price vs Time chart')
+fig = plt.figure(figsize = (12,6))
+plt.plot(df.Close)
+st.pyplot(fig)
+
+# 100 days moving average
+st.subheader('closing price vs Time chart with 150 Moving Average')
+ma150 = df.Close.rolling(150).mean()
+fig = plt.figure(figsize = (12,6))
+plt.plot(ma150, 'green')
+plt.plot(df.Close, 'blue')
+st.pyplot(fig)
+
+#200 days moving average
+st.subheader('closing price vs Time chart with 150 and 300 Moving Average')
+ma150 = df.Close.rolling(150).mean()
+ma300 = df.Close.rolling(300).mean()
+fig = plt.figure(figsize = (12,6))
+plt.plot(ma150, 'green')
+plt.plot(ma300 , 'red')
+plt.plot(df.Close, 'blue')
+st.pyplot(fig)
+
+
+#splitting data into Training and Testing
+
+data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70): int(len(df))])
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler(feature_range=(0,1))
+
+data_training_array = scaler.fit_transform(data_training)
+
+#load model
+model = load_model('keras_model.h5')
+
+
+#testing part
+past_150_days = data_training.tail(150)
+final_df = past_150_days.append(data_testing, ignore_index = True)
+input_data = scaler.fit_transform(final_df)
+
+x_test = []
+y_test = []
+
+for i in range(150, input_data.shape[0]):
+    x_test.append(input_data[i-150: i])
+    y_test.append(input_data[i, 0])
+
+x_test, y_test = np.array(x_test), np.array(y_test)
+y_predicted = model.predict(x_test)
+scaler = scaler.scale_
+
+scale_factor = 1/scaler[0]
+y_predicted = y_predicted * scale_factor
+y_test = y_test * scale_factor
+
+#Final Graph
+st.subheader('Predictions vs Original')
+fig2 = plt.figure(figsize=(12,6))
+plt.plot(y_test , 'blue', label = 'Original Price')
+plt.plot(y_predicted , 'red' , label = 'Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(fig2)
